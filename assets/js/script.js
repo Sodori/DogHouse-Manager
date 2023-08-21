3b901b186c6de21e76e5c3b3949252ec560fe90d
Sodori <jsodori@gmail.com>
2023-08-22 00:10:46 +0200

    Trying to get JS to listen to cell clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 function HelpMe() {
 
 }
@@ -5,28 +7,62 @@ function HelpMe() {
 
 function blackout(section) {
     if (section == 1) {
-        console.log(11)
         document.getElementById("A").style.display = "flex";
+        document.getElementById(1).style.backgroundColor= "yellow"
     } else {
-        console.log(12)
         document.getElementById("A").style.display = "none";
+        document.getElementById(1).style.backgroundColor= "grey"
     }
     
     if (section == 2) {
-        console.log(21)
         document.getElementById("B").style.display = "flex";
+        document.getElementById(2).style.backgroundColor= "yellow"
     } else {
-        console.log(22)
         document.getElementById("B").style.display = "none";
+        document.getElementById(2).style.backgroundColor= "grey"
     }
 
     if (section == 3) {
-        console.log(31)
         document.getElementById("C").style.display = "flex";
+        document.getElementById(3).style.backgroundColor= "yellow"
     } else {
-        console.log(32)
         document.getElementById("C").style.display = "none";
+        document.getElementById(3).style.backgroundColor= "grey"
     }
     return
     
 }
+
+function dimmer(mode) {
+    if (mode == 0) {
+        document.getElementById("dimmer").style.display = "none";
+        document.getElementById("edit").style.display = "none";
+    } else if (mode == 1) {
+        document.getElementById("dimmer").style.display = "block";
+        document.getElementById("edit").style.display = "block";
+        
+    } {
+        
+    }
+    
+}
+
+const Cells = document.getElementsByClassName("Row")
+console.log(Cells)
+
+
+for (let i = 1; i < Cells.length; i++) {
+        let containingElement = document.querySelector('#R' & Cells[i]);
+
+    document.body.addEventListener('click', function( event ){
+        if( containingElement.contains( event.target ) ){
+            console.log("Bing!")
+        } else {
+            console.log("Bong!")
+        }
+});
+}
+
+
+
+});
